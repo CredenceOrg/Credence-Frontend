378d0c5990f9498e20186ff909346dd3f31efdd0
aburex12345 <63457272+aburex12345@users.noreply.github.com>
2026-07-28 09:37:13 +0100

    feat(uiux): add wallet connect header entry point and connected-state spec (#832) (#906)
    Co-authored-by: Realbaji16 <bajiemeks16@gmail.com>

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -2,14 +2,11 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
-import { DEFAULT_LOCALE_CONFIG } from '../config/i18n'
 import en from './locales/en.json'
 import { handleLanguageChanged, setPreviousLng } from './localeBreadcrumb'
 import { getDefaultLocale } from '@/config/i18n'
 
 const defaultLocale = getDefaultLocale()
-
-const defaultLocale = 'en'
 
 i18n
   .use(LanguageDetector)
@@ -18,8 +15,8 @@ i18n
     resources: {
       en: { translation: en },
     },
-    fallbackLng: DEFAULT_LOCALE_CONFIG.defaultLocale,
-    lng: DEFAULT_LOCALE_CONFIG.defaultLocale,
+    fallbackLng: defaultLocale,
+    lng: defaultLocale,
     interpolation: {
       escapeValue: false,
     },
